b09cae4f848bbf1cf80dccc2f4c6ef270eb1dd2f
Potato <toicodedoec@gmail.com>
2021-11-12 19:15:54 -0600

    Update tailwind config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  purge: ['./pages/**/*.tsx', './components/**/*.tsx'],
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {},
   },
